d1d860cf78014d769a3ee607e41dae9c828a4a24
Nicholas Car <nicholas.car@csiro.au>
2020-04-03 13:21:35 +1000

    made SM work (dummy)

--- provworkflow/__init__.py
+++ provworkflow/__init__.py
@@ -1,3 +1,4 @@
 from .prov_reporter import *
 from .workflow import *
 from .block import *
+from .sentencing_method import *
--- provworkflow/sentencing_method.py
+++ provworkflow/sentencing_method.py
@@ -1,18 +1,20 @@
 from provworkflow import ProvWorkflowException
 from provworkflow import Block
 from rdflib import Graph, Namespace, URIRef, BNode, Literal
-from rdflib.namespace import RDFS, XSD
+from rdflib.namespace import RDF, RDFS, XSD
 
 
 class SentencingMethod(Block):
-    def __init__(self, name):
-        super().__init__(name)
-        self.name = name
+    def __init__(self, id):
+        # TODO: use given ID to look up SM URI
+        sm_uri = "http://fake-sm-uri.com"
+        sm_label = "SM x"
+        super().__init__(uri=sm_uri, label=sm_label)
 
     def prov_to_graph(self, g=None):
         g = super().prov_to_graph(g)
 
         # add in type
-        g.add((URIRef(self.uri), RDFS.type, self.PROVWF.SentencingMethod))
+        g.add((URIRef(self.uri), RDF.type, self.PROVWF.SentencingMethod))
 
         return g
