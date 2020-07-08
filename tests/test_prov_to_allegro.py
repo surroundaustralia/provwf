624e61bf4c258c576104383120bea6381e3e9250
david-habgood <david.habgood@surroundaustralia.com>
2020-07-08 15:54:01 +1000

    added test to write to allegro graph

---
+++ tests/test_prov_to_allegro.py
@@ -0,0 +1,17 @@
+from provworkflow import Workflow, Block
+from rdflib import Namespace
+
+def test_Workflow_prov_to_allegro():
+    """Testing that a workflow can write to allegro
+    1 Block within it
+
+    :return: None
+    """
+
+    w = Workflow()
+    b1 = Block()
+    w.blocks.append(b1)
+    w.prov_to_allegro()
+
+if __name__ == "__main__":
+    test_Workflow_prov_to_allegro()
